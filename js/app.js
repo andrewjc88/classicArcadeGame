588cd09f346fc96d4d405c0e5498ed67513ed3ed
andrewjc88 <andrewjc88@gmail.com>
2017-05-22 17:42:45 -0700

    Makes bugs load at random, Next loop bugs

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, Speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = Math.ceil(Math.random()*500);
+    this.y = y;
+    if (y > )
+    this.speed = Math.ceil(Math.random()*200);;
     this.sprite = 'images/enemy-bug.png';
-    this.x = 100;
-    this.y = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -16,8 +18,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = Math.floor(Math.random() * dt);
-    //this.y = Math.floor(Math.random() * diffTime);
+    //this.x + Math.floor(Math.random()) * dt;
+    this.x = this.x + this.speed * dt;
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,7 +34,7 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.x = 405;
+    this.x = 202;
     this.y = 370;
 }
 
@@ -43,6 +46,7 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Keeps player inside of game board and controls player movement size.
 Player.prototype.handleInput = function(inputKeyCode) {
     if (inputKeyCode === 'left' && this.x >= 101) {
         this.x -= 101;
@@ -60,11 +64,15 @@ Player.prototype.handleInput = function(inputKeyCode) {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+
+var bugOne = new Enemy(-110, 83, 20);
+var bugTwo = new Enemy(-110, 166, 200);
+var bugThree = new Enemy(-110, 249, 50);
+var allEnemies = [bugOne, bugTwo, bugThree];
+
 // Place the player object in a variable called player
 
-var allEnemies = [];
-
-var player = new Player;
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
